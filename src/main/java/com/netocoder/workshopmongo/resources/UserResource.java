7abe6134cb4aa05fb711d97a554985f7bd7499d0
NetoCoder <netogerbi2@hotmail.com>
2019-03-13 19:55:05 -0300

    Conecting to MongoDB with Repository and Service Pattern

--- src/main/java/com/netocoder/workshopmongo/resources/UserResource.java
+++ src/main/java/com/netocoder/workshopmongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.netocoder.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netocoder.workshopmongo.domain.User;
+import com.netocoder.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User(1,"Maria Silva","maria@email.com");
-		User joao = new User(1,"João Silva","joao@email.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, joao));
+		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
