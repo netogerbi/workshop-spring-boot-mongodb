147500350c5a3b01d8460937de57d60a4a9ae756
NetoCoder <netogerbi2@hotmail.com>
2019-03-18 21:26:50 -0300

    Getting an user by id and treating object not found exception

--- src/main/java/com/netocoder/workshopmongo/services/UserService.java
+++ src/main/java/com/netocoder/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.netocoder.workshopmongo.domain.User;
 import com.netocoder.workshopmongo.repository.UserRepository;
+import com.netocoder.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!"));
+		return user;
 	}
 	
 }
