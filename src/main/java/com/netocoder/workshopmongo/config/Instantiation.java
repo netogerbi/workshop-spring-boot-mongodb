1cc49a83733b891015919f67e4eb49b28cdaa2ff
NetoCoder <netogerbi2@hotmail.com>
2019-03-21 21:58:44 -0300

    Creating Post entity with nested User

--- src/main/java/com/netocoder/workshopmongo/config/Instantiation.java
+++ src/main/java/com/netocoder/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.netocoder.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.netocoder.workshopmongo.domain.Post;
 import com.netocoder.workshopmongo.domain.User;
+import com.netocoder.workshopmongo.repository.PostRepository;
 import com.netocoder.workshopmongo.repository.UserRepository;
 
 
@@ -16,15 +20,29 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		User u1 = new User(null,"NetoCoder","netocoder@hotmail.com");
 		User u2 = new User(null, "TatiCoder", "tati@coder");
 		User u3 = new User(null, "John of the Night", "jotn@coder.com");
 		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", u1);
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u2);
+		
 		userRepository.deleteAll();//limpar o mongodb
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		
+		postRepository.deleteAll();
+		postRepository.saveAll(Arrays.asList(p1,p2));
+				
 		
 	}
 	
---
+++ src/main/java/com/netocoder/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.netocoder.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.netocoder.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
+
