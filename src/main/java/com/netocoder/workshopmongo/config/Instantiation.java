28120150f62fabe355da0da4ae032ded206db0fe
NetoCoder <netogerbi2@hotmail.com>
2019-03-27 19:27:30 -0300

    projecting author data with AuthorDTO

--- src/main/java/com/netocoder/workshopmongo/config/Instantiation.java
+++ src/main/java/com/netocoder/workshopmongo/config/Instantiation.java
@@ -10,12 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 import com.netocoder.workshopmongo.domain.Post;
 import com.netocoder.workshopmongo.domain.User;
+import com.netocoder.workshopmongo.dto.AuthorDTO;
 import com.netocoder.workshopmongo.repository.PostRepository;
 import com.netocoder.workshopmongo.repository.UserRepository;
 
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
@@ -34,16 +35,15 @@ public class Instantiation implements CommandLineRunner{
 		User u2 = new User(null, "TatiCoder", "tati@coder");
 		User u3 = new User(null, "John of the Night", "jotn@coder.com");
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u2);
-		
 		userRepository.deleteAll();//limpar o mongodb
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u2));
 		
 		postRepository.deleteAll();
 		postRepository.saveAll(Arrays.asList(p1,p2));
 				
-		
 	}
 	
 }
