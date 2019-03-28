bcdeac9f657e720eb4761f695c2134a01466d7b2
NetoCoder <netogerbi2@hotmail.com>
2019-03-27 21:25:34 -0300

    Simple consult using query methods

--- src/main/java/com/netocoder/workshopmongo/resources/PostResource.java
+++ src/main/java/com/netocoder/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.netocoder.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netocoder.workshopmongo.domain.Post;
+import com.netocoder.workshopmongo.resources.util.URL;
 import com.netocoder.workshopmongo.services.PostService;
 
 @RestController
@@ -21,6 +25,13 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
  		Post obj = postService.findById(id);
  		return ResponseEntity.ok().body(obj);
+ 	}
+	
+	@RequestMapping(value="/title", method=RequestMethod.GET)
+ 	public ResponseEntity <List<Post>> findByTitle( @RequestParam(value="text", defaultValue="") String s) {
+		String text = URL.decodeParam(s);
+		List<Post> list = postService.finByTitle(text);
+ 		return ResponseEntity.ok().body(list);
  	}
 	
 }
--- src/main/java/com/netocoder/workshopmongo/services/PostService.java
+++ src/main/java/com/netocoder/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.netocoder.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,10 @@ public class PostService {
 	public Post findById(String id) {
 		Post obj = postRepository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!"));
 		return obj;
+	}
+	
+	public List<Post> finByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
